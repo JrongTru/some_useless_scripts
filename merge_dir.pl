6bd1e2e5ad8a88471eebf08f9ca18f6615a87177
Tru Jrong <jrongtru@icloud.com>
2018-02-26 17:46:03 +0800

    modify scan mode and correct some syntax error.

--- merge_dir.pl
+++ merge_dir.pl
@@ -1,5 +1,8 @@
 #!/usr/bin/perl
 
+use v5.22.1;
+use strict;
+
 my $din;
 my $dout;
 foreach (@ARGV)
@@ -9,7 +12,6 @@ foreach (@ARGV)
 }
 
 die "input directory missing" unless $din;
-die "output directory missing" unless $dout;
 
 while ( $din =~ s/(.*)\/$/\1/ ){}
 while ( $dout =~ s/(.*)\/$/\1/ ){}
@@ -20,16 +22,23 @@ print "$din\n$dout\n";
 
 opendir(TRU, $din) or die "error: $!";
 my @a = readdir TRU;
-my @dirs;
+my (@dirs, @d);
 closedir TRU;
 my $i = 1;
 print "entering first loop!\n";
 foreach (@a)
 {
 	my $lpdir = "$din/$_";
-	if( -d $lpdir ? ($lpdir !~ m/.*?\/(\.)+(.*)?/): 0)#&& (-d $_) )
+
+	next if ($_ =~ m/^\.+$/);
+
+	if ( !(-d $lpdir) )
 	{
-		print "read the no.[$i] $lpdir directory!\n";
+		say "$lpdir" if $lpdir =~ m/(.*)?\.pl$/ ;
+	}
+	elsif( -d $lpdir ? ($lpdir !~ m/^(.*)?\/\.+$/): 0)#&& (-d $_) )
+	{
+		#print "read the no.[$i] $lpdir directory!\n";
 		opendir(SUBDIR, $lpdir) or die "error: $!";
 		@d = readdir SUBDIR;
 		closedir SUBDIR;
@@ -44,20 +53,21 @@ foreach (@a)
 				#print "|--------------------------------------------|\n";
 				next NEXT_ROUND;
 			}
-			print "\n|--------------------------------------------|\n";
+			#print "\n|--------------------------------------------|\n";
+			say $path if $path =~ m/(.*)?\.sample$/;
 			#print "$path is a file!\n";
-			$path =~ tr/\//_/;
+			#$path =~ tr/\//_/;
 			#`echo $path $dout/$fname`;
-			print "$path moved\n";
-			print "|--------------------------------------------|\n";
+			#print "$path moved\n";
+			#print "|--------------------------------------------|\n";
 		}
 		my $cnt = @dirs;
-		print "[$cnt] directories in array!\n";
+		#print "[$cnt] directories in array!\n";
 		if( $cnt > 0 )
 		{
-			@d = [];
+			@d = undef;
 			my $tmp = pop @dirs;
-			print "reading sub directory: $tmp\n";
+			#print "reading sub directory: $tmp\n";
 			opendir( SUBDIR, $tmp) or die "error: $!";
 			@d = readdir SUBDIR;
 			closedir SUBDIR;
