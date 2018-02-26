9983337d88472859b91efb73d62b4855ed29be9f
Tru Jrong <jrongtru@icloud.com>
2018-02-26 18:26:01 +0800

    set input directory path to absolute path

--- merge_dir.pl
+++ merge_dir.pl
@@ -2,6 +2,7 @@
 
 use v5.22.1;
 use strict;
+use Cwd;
 
 my $din;
 my $dout;
@@ -12,6 +13,8 @@ foreach (@ARGV)
 }
 
 die "input directory missing" unless $din;
+$din = &Cwd::abs_path($din);
+$dout = &Cwd::abs_path($dout) if $dout;
 
 while ( $din =~ s/(.*)\/$/\1/ ){}
 while ( $dout =~ s/(.*)\/$/\1/ ){}
@@ -29,23 +32,22 @@ print "entering first loop!\n";
 foreach (@a)
 {
 	my $lpdir = "$din/$_";
+	#say $lpdir;
 
 	next if ($_ =~ m/^\.+$/);
 
-	if ( !(-d $lpdir) )
-	{
-		say "$lpdir" if $lpdir =~ m/(.*)?\.pl$/ ;	#only print perl file in the directory.
-	}
-	elsif( -d $lpdir ? ($lpdir !~ m/^(.*)?\/\.+$/): 0)#&& (-d $_) )
+	if( -d $lpdir )#? ($lpdir !~ m/^((.*)?\/)?\.+$/): 0)#&& (-d $_) )
 	{
 		#print "read the no.[$i] $lpdir directory!\n";
 		opendir(SUBDIR, $lpdir) or die "error: $!";
 		@d = readdir SUBDIR;
+		#say join(', ', @d);
 		closedir SUBDIR;
 		NEXT_ROUND: foreach (@d)
 		{
-			next NEXT_ROUND if $_ =~ /^(\.)+/;
+			next NEXT_ROUND if $_ =~ /^(\.)+$/;
 			my $path = "$lpdir/$_";
+			#say $path;
 			if( -e $path && -d $path )
 			{
 				#print "$path is a directory.\n";
@@ -75,6 +77,10 @@ foreach (@a)
 			#print "|--------------------------------------------|\n";
 			goto NEXT_ROUND;
 		}
+	}
+	else
+	{
+		say "$lpdir" if $lpdir =~ m/(.*)?\.pl$/ ;	#only print perl file in the directory.
 	}
 }
 
